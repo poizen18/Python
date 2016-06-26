23af38e66a73ab29045263a7afc02c50428f4407
Brian Spaulding <bspaulding@live.com>
2016-06-26 12:01:52 -0700

    Modifying search string

--- Misc-Scripts/irc_bots/donald_irc_bot.py
+++ Misc-Scripts/irc_bots/donald_irc_bot.py
@@ -15,15 +15,12 @@ irc.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :Annoying little bot\n"
 irc.send("NICK "+ botnick +"\n")
 irc.send("JOIN "+ channel +"\n")
 
-trumps = ['trump','Trump', '!trump', '!Trump']
-
 while 1:
    text=irc.recv(2040)
    print text
    if text.find('PING') != -1:
       irc.send('PONG ' + text.split() [1] + '\r\n')
-   for trump in trumps:
-      if text.find(trump) !=-1:
-         quote = random.choice(list(open('donald_quotes.txt')))
-         time.sleep(2)
-         irc.send('PRIVMSG ' + channel + ' :' + quote + '\r\n')
+   if text.lower().find('trump') !=-1:
+      quote = random.choice(list(open('donald_quotes.txt')))
+      time.sleep(2)
+      irc.send('PRIVMSG ' + channel + ' :' + quote + '\r\n')
