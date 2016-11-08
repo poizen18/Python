ea48b8baecffd588d6b54e0d2e41a0246964ca49
Brian Spaulding <bspaulding@live.com>
2016-11-08 15:56:28 -0800

    adding new combined IRC bot to github

---
+++ turing/turing_bot.py
@@ -0,0 +1,273 @@
+#!/usr/local/bin/python
+
+#+-----------------------------------------------------------------------+
+#| File Name: turing_bot.py                                              |
+#+-----------------------------------------------------------------------+   
+#| Description: Turing watches an IRC channel and responds to prompts    |
+#+-----------------------------------------------------------------------+
+#| Usage: Use ++ or -- to hand out or take away karma (e.g. wyldbrian++) | 
+#|        Use !rank to check an items karma rank (e.g. !rank wyldbrian)  |
+#|        Use !top to see the top 5 items by karma                       |
+#|        Use !bottom to see the bottom 5 items by karma                 |
+#!        Use !weather City,State to check weather                       |
+#+-----------------------------------------------------------------------+
+#| Authors: wyldbrian                                                    |
+#+-----------------------------------------------------------------------+
+#| Date: 2016-11-08                                                      |
+#+-----------------------------------------------------------------------+
+#| Version: 1.0                                                          |
+#+-----------------------------------------------------------------------+
+
+####################################################
+#             Import necessary modules             #
+####################################################
+
+import ssl
+import sys
+import json
+import time
+import socket
+import urllib2
+import logging
+import threading
+
+####################################################
+#             Set IRC connection values            #
+####################################################
+
+server = "localhost"
+channel = "#smalltalk"
+botnick = "turing"
+
+####################################################
+#              Set logging parameters              #
+####################################################
+
+logfile = '/var/log/karmabot.log'
+loglevel = logging.INFO
+logformat = '%(asctime)s - %(levelname)s - %(message)s'
+logging.basicConfig(filename=logfile,format=logformat,level=loglevel)
+
+####################################################
+#          Create karma save/load process          #
+####################################################
+
+karma_val = []
+karma_num = []
+
+def karmaload():
+	global karma_val
+	global karma_num
+	load_val = open("karma_val.json")
+	load_num = open("karma_num.json")
+	karma_val = json.loads(load_val.read())
+	karma_num = json.loads(load_num.read())
+	load_val.close()
+	load_num.close()
+
+def karmasave():
+	threading.Timer(30,karmasave).start()
+	global karma_val
+	global karma_num
+	save_val = file("karma_val.json", "w")
+	save_num = file("karma_num.json", "w")
+	save_val.write(json.dumps(karma_val))
+	save_num.write(json.dumps(karma_num))
+	save_val.close()
+	save_num.close()
+
+try:
+	karmaload()
+except:
+	logging.CRITICAL('Karmaload failed, exiting')
+	sys.exit()
+else:
+	karmasave()
+
+
+
+####################################################
+#          Build karma functions for IRC           #
+####################################################
+
+def karmaup():
+	try:
+		karma_up = (text.split("++")[0]).split(":")[2].rsplit(None,1)[-1]
+	except IndexError:
+		message = "What would you like to give Karma to? (e.g. Karmabot++)"
+		irc.send('PRIVMSG ' + channel + ' :' + message + '\r\n')
+		return
+	if karma_up in karma_val:
+		idx = karma_val.index(karma_up)
+		num = karma_num[idx]
+		karma_num[idx] = int(num) + 1
+		user = (text.split(":")[1]).split("!")[0]
+		logmsg = user + " gave karma to " + karma_up + " (++)"
+		logging.info(logmsg)
+	elif karma_up not in karma_val:
+		karma_val.append(karma_up)
+		karma_num.append(1)
+		user = (text.split(":")[1]).split("!")[0]
+		logmsg = user + " gave karma to " + karma_up + " (++)"
+		logging.info(logmsg)
+		
+def karmadown():
+	try:
+		karma_down = (text.split("--")[0]).split(":")[2].rsplit(None,1)[-1]
+	except IndexError:
+		message = "What would you like to take Karma away from? (e.g. Karmabot--)"
+		irc.send('PRIVMSG ' + channel + ' :' + message + '\r\n')
+		return
+	if karma_down in karma_val:
+		idx = karma_val.index(karma_down)
+		num = karma_num[idx]
+		karma_num[idx] = int(num) - 1
+		user = (text.split(":")[1]).split("!")[0]
+		logmsg = user + " took karma away from " + karma_down + " (--)"
+		logging.info(logmsg)
+	elif karma_down not in karma_val:
+		karma_val.append(karma_down)
+		karma_num.append(-1)
+		user = (text.split(":")[1]).split("!")[0]
+		logmsg = user + " took karma away from " + karma_down + " (--)"
+		logging.info(logmsg)
+		
+def karmarank():
+	try:
+		rank = (text.split(':!rank')[1]).strip()
+	except IndexError:
+		message = "What would you like to check the rank of? (e.g. !rank Karmabot)"
+		irc.send('PRIVMSG ' + channel + ' :' + message + '\r\n')
+		return
+	if rank in karma_val:
+		idx = karma_val.index(rank)
+		num = karma_num[idx]
+		message = (rank + " has " + str(num) + " points of karma!")
+		irc.send('PRIVMSG ' + channel + ' :' + message + '\r\n')
+	elif rank not in karma_val:
+		message = (rank + " doesn't have any karma yet!")
+		irc.send('PRIVMSG ' + channel + ' :' + message + '\r\n')
+		
+def topkarma():
+	top_results = sorted(zip(karma_num,karma_val), reverse=True)[:5]
+	irc.send('PRIVMSG ' + channel + ' :' + "## TOP 5 KARMA RECIPIENTS ##" + '\r\n')
+	for (x,y) in top_results:
+		message = (y + ": " + str(x))
+		irc.send('PRIVMSG ' + channel + ' :' + message + '\r\n')
+		
+def bottomkarma():
+	top_results = sorted(zip(karma_num,karma_val), reverse=False)[:5]
+	irc.send('PRIVMSG ' + channel + ' :' + "## BOTTOM 5 KARMA RECIPIENTS ##" + '\r\n')
+	for (x,y) in top_results:
+		message = (y + ": " + str(x))
+		irc.send('PRIVMSG ' + channel + ' :' + message + '\r\n')		
+
+####################################################
+#          Build weather function for IRC          #
+####################################################
+
+def weathercheck():
+	try:
+		city = (text.split("!weather")[1]).split(",")[0].lstrip().replace(" ","_")
+		state = (text.split("!weather")[1]).split(",")[1].strip().replace(" ","_")
+	except IndexError:
+		message = "What city's weather would you like to check? (e.g. !weather Bend,OR)"
+		irc.send('PRIVMSG ' + channel + ' :' + message + '\r\n')
+		return
+	api = urllib2.urlopen('http://api.wunderground.com/api/0c6e61e8fe3d5798/geolookup/conditions/q/%s/%s.json' % (state,city))
+	json_string = api.read()
+	parsed_json = json.loads(json_string)
+	try:
+		temp = parsed_json['current_observation']['temperature_string']
+		location = parsed_json['location']['city']
+		condition = parsed_json['current_observation']['weather']
+	except:
+		message = ("What city's weather would you like to check? (e.g. !weather Bend,OR)")
+		irc.send('PRIVMSG ' + channel + ' :' + message + '\r\n')
+		return
+	message = "The weather in %s is currently showing %s with a temperature of %s" %(location,condition,temp) 
+	irc.send('PRIVMSG ' + channel + ' :' + message + '\r\n')
+	api.close()
+
+####################################################
+#   Build and run function for earthquake checks   #
+####################################################	
+
+quakelist = []
+
+def quakecheck():
+	threading.Timer(900,quakecheck).start()
+	quake_feed = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_hour.geojson'
+	try:
+		quake_call = urllib2.urlopen(quake_feed)
+	except:
+		message = "Caught exception trying to connect to usgs api"
+		logging.warning(message)
+		return
+	quake_output = quake_call.read()
+	quake_call.close
+	quake_dict = json.loads(quake_output)
+	global quakelist
+	for quake in quake_dict['features']:
+		if quake['id'] not in quakelist:
+			quakelist.append(quake['id'])
+			mag = quake['properties']['mag']
+			location = quake['properties']['place']
+			details = quake['properties']['url']
+			message = "%s magnitude earthquake detected %s - %s" %(mag,location,details)
+			try:
+				irc.send('PRIVMSG ' + channel + ' :' + message + '\r\n')
+			except:
+				message = "Caught exception trying to post quake info to IRC"
+				logging.warning(message)
+			logging.info(message)
+			
+####################################################
+#            Build IRC connect function            #
+####################################################
+
+def connect():
+	global irc_sock
+	global irc
+	irc_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	irc = ssl.wrap_socket(irc_sock)
+	irc.connect((server, 6667))
+	irc.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :A computer would deserve to be called intelligent if it could deceive a human into believing that it was human.\n")
+	irc.send("NICK "+ botnick +"\n")
+	irc.send("JOIN "+ channel +"\n")
+
+####################################################
+# Watch IRC chat for key values and run functions  #
+####################################################	
+
+connect()
+
+quakecheck()
+
+while 1:
+	text=irc.recv(1024)
+	if text.find('PING') != -1:
+		irc.send('PONG ' + text.split() [1] + '\r\n')
+	elif text.find('++') !=-1 and text.find(channel) !=-1:
+		karmaup()
+	elif text.find('--') !=-1 and text.find(channel) !=-1:
+		karmadown()
+	elif text.find('!rank') !=-1 and text.find(channel) !=-1:
+		karmarank()
+	elif text.find('!top') !=-1 and text.find(channel) !=-1:
+		topkarma()
+	elif text.find('!bottom') !=-1 and text.find(channel) !=-1:
+		bottomkarma()
+	elif text.find('!weather') !=-1 and text.find(channel) !=-1:
+		weathercheck()
+	elif text.find('!help') !=-1 and text.find(channel) !=-1:
+		time.sleep(.5)
+		help()
+	elif len(text) == 0:
+		while 1:
+			try:
+				time.sleep(300)
+				connect()
+			except:
+				continue
+			break
